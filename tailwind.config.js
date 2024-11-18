458e0f38e4c8a535cd4634e3d01be15fb4e92e7d
Sobitov701 <abdurasul03sobitaliyev>
2024-11-18 13:19:24 +0300

    all add

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./*.{html, css}"],
+  theme: {
+    extend: {
+      colors: {
+        "dark-green": "#079211",
+        "custom-grey": "#808080",
+      },
+    },
+  },
+  plugins: [],
+};
+
